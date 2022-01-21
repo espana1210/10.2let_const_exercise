80554b689d49e0c80cdcbea2675f9d1d97926a21
Guo <espana1210@gmail.com>
2022-01-21 15:04:48 -0800

    my exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1
+// var can be reassigned and redeclared
+// let can not be redeclared
+// var can be assessd by a hoisted variable
+
+// 2
+// var can be reassigned and redeclared
+// var can be assessd by a hoisted variable
+// const can not be redeclared and reassigned
+
+// 3
+// let can be reassigned
+// const can not be redeclared and reassigned
+
+// 4
+// variables are lifting or 'hoisted' to the top of the scope
